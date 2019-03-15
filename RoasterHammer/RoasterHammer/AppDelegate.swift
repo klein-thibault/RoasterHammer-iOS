9a4acd2cc84219f563f5cf11b16c071b1ee6c5ee
ThibaultKlein <thibault@prolificinteractive.com>
2019-03-15 14:54:35 -0400

    Used shared for dependency manager singleton in App Delegate

--- RoasterHammer/RoasterHammer/AppDelegate.swift
+++ RoasterHammer/RoasterHammer/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        RoasterHammerDependencyManager().startApplication(window: window)
+        RoasterHammerDependencyManager.shared.startApplication(window: window)
 
         return true
     }
