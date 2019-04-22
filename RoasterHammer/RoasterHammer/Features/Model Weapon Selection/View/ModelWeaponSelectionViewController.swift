8c27a2b881d354fa5aa7243dd03609198769e549
ThibaultKlein <thibault@prolificinteractive.com>
2019-04-22 16:26:24 -0400

    Updated model weapon selection view to display weapon buckets and weapons inside each

--- RoasterHammer/RoasterHammer/Features/Model Weapon Selection/View/ModelWeaponSelectionViewController.swift
+++ RoasterHammer/RoasterHammer/Features/Model Weapon Selection/View/ModelWeaponSelectionViewController.swift
@@ -34,19 +34,44 @@ final class ModelWeaponSelectionViewController: ModelWeaponSelectionLayoutViewCo
 }
 
 extension ModelWeaponSelectionViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return selectedModel.model.weaponBuckets.count
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        let weaponBucket = selectedModel.model.weaponBuckets[section]
+        return weaponBucket.weapons.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: SingleLabelTableViewCell = tableView.dequeueIdentifiableCell(for: indexPath)
+        let weaponBucket = selectedModel.model.weaponBuckets[indexPath.section]
+        let weapon = weaponBucket.weapons[indexPath.row]
+
+        cell.setupWithText(weapon.name)
 
         return cell
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let sectionName = selectedModel.model.weaponBuckets[section].name
+        let frame = CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 44.0)
+        let headerView = TableViewHeaderLabel(frame: frame)
+
+        headerView.setupWithTitle(sectionName)
+
+        return headerView
     }
 }
 
 extension ModelWeaponSelectionViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44.0
+    }
 
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44.0
+    }
 }
 
 extension ModelWeaponSelectionViewController: ModelWeaponSelectionView {
